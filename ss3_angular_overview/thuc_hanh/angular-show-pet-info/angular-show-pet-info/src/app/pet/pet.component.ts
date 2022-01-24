770f59627aa50c374c6bdb1ee222f9d3a901803e
NguyenThanhDuong113bn <nguyenthanhduong113bn@gmail.com>
2022-01-24 16:36:34 +0700

    update

---
+++ ss3_angular_overview/thuc_hanh/angular-show-pet-info/angular-show-pet-info/src/app/pet/pet.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet.component.css']
+})
+export class PetComponent implements OnInit {
+  pet: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
