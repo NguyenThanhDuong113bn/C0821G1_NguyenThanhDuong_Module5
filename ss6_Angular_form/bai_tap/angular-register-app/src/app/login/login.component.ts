e449f5260f64b4b0d2a2bfac1945e5d64e22bdb8
NguyenThanhDuong113bn <nguyenthanhduong113bn@gmail.com>
2022-02-14 22:08:57 +0700

    update

---
+++ ss6_Angular_form/bai_tap/angular-register-app/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+
+  constructor() {
+    // @ts-ignore
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value));
+  }
+}
